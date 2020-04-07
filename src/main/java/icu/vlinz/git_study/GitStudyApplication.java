74310537079d71dea0de29e9d7a6a3782885bfea
vlinz <vlinz@foxmail.com>
2020-04-07 21:37:46 +0800

    new git

---
+++ src/main/java/icu/vlinz/git_study/GitStudyApplication.java
@@ -0,0 +1,20 @@
+package icu.vlinz.git_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class GitStudyApplication {
+    @RequestMapping("/git")
+    public String Git(){
+        return "Hello Git!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/icu/vlinz/git_study/GitStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.vlinz.git_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
